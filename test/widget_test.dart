66ec61b26ee978c45945a33d0cb7e79ab912165f
hapo-longnd <longnd@haposoft.com>
2023-02-28 17:32:46 +0700

    test widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,48 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:demo_riverpod/products/pages/favorite_list_page.dart';
+import 'package:demo_riverpod/products/product_page.dart';
+import 'package:demo_riverpod/products/widgets/card_item_product_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:demo_riverpod/main.dart';
-
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+  testWidgets('Test favorite page', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const ProviderScope(
+        child: MaterialApp(
+          home: Scaffold(
+            body: FavoriteListPage(),
+          ),
+        ),
+      ),
+    );
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
     await tester.pump();
+    expect(find.byType(SpinKitCircle), findsOneWidget);
+    await tester.pump(const Duration(seconds: 1));
+    expect(find.byType(SpinKitCircle), findsNothing);
+    expect(find.text("No data"), findsOneWidget);
+  });
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('Test product page', (WidgetTester tester) async {
+    await tester.pumpWidget(
+      const ProviderScope(
+        child: MaterialApp(
+          home: Scaffold(
+            body: ProductsPage(),
+          ),
+        ),
+      ),
+    );
+
+    // await tester.pump();
+    // expect(find.byType(SpinKitCircle), findsOneWidget);
+    // await tester.pump(const Duration(seconds: 1));
+    // expect(find.byType(SpinKitCircle), findsNothing);
+    // expect(tester.widgetList(find.byType(CardItemProductWidget)), findsWidgets);
   });
 }
